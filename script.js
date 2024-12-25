c076994a7c8e4a735536f83a09001fdcf13dd4a2
Mikrosat <seidlermateusz@gmail.com>
2024-12-25 14:32:08 +0100

    Mutting sounds, categories, better organizing code

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 console.log('Please dont use console to hack, you are ruining your own fun!');
-var slogan = "Bez pracy nie ma kołaczy";
+
+var slogansBase = [
+    {
+        title: "Powiedzenia",
+        items: 
+        [
+            "Bez pracy nie ma kołaczy",
+            "Po burzy wychodzi słońce",
+            "Co dwie głowy to nie jedna"
+        ]
+    },
+    {
+        title: "Gry komputerowe",
+        items:
+        [
+            "Grand Theft Auto V",
+            "Minecraft",
+            "Roblox"
+        ]
+    }
+];
+
+var chosenCategory = slogansBase[random(0, slogansBase.length - 1)];
+var slogan = chosenCategory.items[random(0, chosenCategory.items.length - 1)];
+var sloganCategory = chosenCategory.title;
 slogan = slogan.toUpperCase();
 
+var muted = false;
 var sloganLength = slogan.length;
 var missed = 0;
 var hashedSlogan = "";
@@ -12,44 +37,23 @@ for(i=0; i<sloganLength; i++){
     if(slogan.charAt(i)==" ") hashedSlogan += " ";
     else hashedSlogan += "-";
 }
-var letters = new Array(35);
+var letters = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
 
-letters[0] = "A";
-letters[1] = "Ą";
-letters[2] = "B";
-letters[3] = "C";
-letters[4] = "Ć";
-letters[5] = "D";
-letters[6] = "E";
-letters[7] = "Ę";
-letters[8] = "F";
-letters[9] = "G";
-letters[10] = "H";
-letters[11] = "I";
-letters[12] = "J";
-letters[13] = "K";
-letters[14] = "L";
-letters[15] = "Ł";
-letters[16] = "M";
-letters[17] = "N";
-letters[18] = "Ń";
-letters[19] = "O";
-letters[20] = "Ó";
-letters[21] = "P";
-letters[22] = "Q";
-letters[23] = "R";
-letters[24] = "S";
-letters[25] = "Ś";
-letters[26] = "T";
-letters[27] = "U";
-letters[28] = "V";
-letters[29] = "W";
-letters[30] = "X";
-letters[31] = "Y";
-letters[32] = "Z";
-letters[33] = "Ż";
-letters[34] = "Ź";
+function muteBtnClicked(){
+    if(muted){
+        document.getElementById("muteBtn").innerHTML = '<img src="img/unmuteIcon.png" class="muteIcon" onclick="muteBtnClicked()"/>'
+        muted = false;
+    }
+    else{
+        document.getElementById("muteBtn").innerHTML = '<img src="img/muteIcon.png" class="muteIcon" onclick="muteBtnClicked()"/>'
+        muted = true;
+    }
+}
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function writeSlogan(){
+    document.getElementById('sloganCategory').innerHTML = '<h5>Kategoria: '+sloganCategory+'</h5>';
     document.getElementById('gameBoard').innerHTML = hashedSlogan;
 }
 function start(){
@@ -57,7 +61,7 @@ function start(){
     for (i=0; i<35; i++)
     {
         var element = "let"+i;
-        divContent = divContent + '<div class="letter" id="'+element+'" onclick="checkNo('+i+')">'+letters[i]+'</div>';
+        divContent = divContent + '<div class="letter" id="'+element+'" onclick="checkNo('+i+')">'+letters.charAt(i)+'</div>';
         if((i+1) % 7 == 0) divContent += '<div style="clear: both"></div>';
     }
 
@@ -72,16 +76,16 @@ function checkNo(no){
     var hitted = false;
     for(i=0; i<sloganLength; i++)
     {
-        if(slogan.charAt(i) == letters[no])
+        if(slogan.charAt(i) == letters.charAt(no))
         {
-            hashedSlogan = hashedSlogan.setChar(i,letters[no]);
+            hashedSlogan = hashedSlogan.setChar(i,letters.charAt(no));
             hitted = true;
         }
     }
 
     var buttonID = "let" + no;
     if(hitted == true){
-        hittedSound.play();
+        if(!muted) hittedSound.play();
         document.getElementById(buttonID).style.background = "#005500";
         document.getElementById(buttonID).style.color = "#00CC00";
         document.getElementById(buttonID).style.border = "3px solid #00CC00";
@@ -90,13 +94,15 @@ function checkNo(no){
         writeSlogan();
     }
     else{
-        missedSound.play();
+        if(!muted) missedSound.play();
         document.getElementById(buttonID).style.background = "#550000";
         document.getElementById(buttonID).style.color = "#CC0000";
         document.getElementById(buttonID).style.border = "3px solid #CC0000";
         document.getElementById(buttonID).style.cursor = "default";
         document.getElementById(buttonID).setAttribute("onclick",";"); //destroying onclick
         missed++;
+        if(missed>9)
+            missed = 9;
         document.getElementById("gallow").innerHTML = '<img src="/img/s'+missed+'.jpg" />';
     }
     //win
